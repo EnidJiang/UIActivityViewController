0e3a9dae8ea696f4819c67dd9995c070de14b66d
蒋旖旎 <jnjiang517@163.com>
2018-04-04 09:29:27 +0800

    Add URL , Text ,Image Share

--- UIActivityViewController/ViewController.swift
+++ UIActivityViewController/ViewController.swift
@@ -15,11 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func shareURL(_ sender: Any) {
+        let URLstring =  String(format:"https://itunes.apple.com/cn/app/%E6%98%93%E7%83%B9%E9%A5%AA/id1360197713?mt=8")
+        let urlToShare = URL(string:URLstring)
+        let title = "title to be shared"
+        let activityViewController = UIActivityViewController(
+            activityItems: [title,urlToShare!],
+            applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        //so that ipads won't crash
+        present(activityViewController,animated: true,completion: nil)
     }
-
+    
+    @IBAction func shareText(_ sender: Any) {
+        let text = "Text to be shared"
+        let activityViewController = UIActivityViewController(activityItems:[text],applicationActivities:nil)
+       activityViewController.popoverPresentationController?.sourceView = self.view
+        present(activityViewController,animated: true,completion: nil)
+    }
+    
+    @IBAction func shareImage(_ sender: Any) {
+        let image = #imageLiteral(resourceName: "myImage")
+        let activityViewController = UIActivityViewController(activityItems:[image],applicationActivities:nil)
+       activityViewController.popoverPresentationController?.sourceView = self.view
+        present(activityViewController,animated: true,completion: nil)
+    }
+    
+    
 
 }
 
